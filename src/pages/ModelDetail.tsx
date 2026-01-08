88e2a22a3c6485e9ca7b0847b55881af0e81f307
jesustissera <jesustissera@gmail.com>
2026-01-08 12:29:51 -0300

    Request a api: detalle del modelo

--- src/pages/ModelDetail.tsx
+++ src/pages/ModelDetail.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Menu from "../components/Menu";
+
 const ModelDetail = () => {
+    const { id } = useParams();
+    const [model, setModel] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://challenge.egodesign.dev/api/models/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setModel(data);
+            })
+            .catch((error) => console.error("Error fetching model:", error));
+    }, [id]);
+
+    if (!model) return <div>Cargando...</div>;
+
     return (
-        <div>Model Detail Page</div>
-    )
-}
+        <div>
+            <Menu />
+            <h1>{model.name}</h1>
+            <p>Year: {model.year}</p>
+            <p>Price: ${model.price}</p>
+        </div>
+    );
+};
 export default ModelDetail;
