d47b24f01be9b18a478b78be723244dc9ad61e4a
jesustissera <jesustissera@gmail.com>
2026-01-08 12:14:10 -0300

    Request a api: listado de modelos

--- src/components/ModelsGrid.tsx
+++ src/components/ModelsGrid.tsx
@@ -1,6 +1,22 @@
-const ModelsGrid = () => {
+import { Link } from "react-router-dom";
+
+const ModelsGrid = ({models}) => {
     return (
-        <div>Models Grid Component</div>
+    <section>
+      <h2>Grilla</h2>
+
+      <ul>
+        {models.map((model) => (
+          <li key={model.id}>
+            <Link to={`/model/${model.id}`}>
+              <div>{model.name}</div>
+              <div>Price: {model.price}</div>
+              <div>Year: {model.year}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
     )
 }
 export default ModelsGrid;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
 import Filters from "../components/Filters";
 import Menu from "../components/Menu";
 import ModelsGrid from "../components/ModelsGrid";
 import Sort from "../components/Sort";
 
 const Home = () => {
+  const [models, setModels] = useState(null);
+
+  useEffect(() => {
+    fetch("https://challenge.egodesign.dev/api/models/")
+      .then((response) => response.json())
+      .then((data) => {
+        setModels(data);
+      })
+      .catch((error) => console.error("Error fetching models:", error));
+  }, []);
+
+  if(!models) return <div>Cargando...</div>;
+
   return (
     <>
       <Menu />
       <div>Home Page</div>
       <Filters />
       <Sort />
-      <ModelsGrid />
+      <ModelsGrid models={models} />
     </>
   );
 };
