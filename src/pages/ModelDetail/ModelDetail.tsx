7aadf7fa52a2f44b7454764baa0fdb5f74896949
jesustissera <jesustissera@gmail.com>
2026-01-09 18:56:37 -0300

    Completo hero para el detalle del modelo

--- src/pages/ModelDetail/ModelDetail.tsx
+++ src/pages/ModelDetail/ModelDetail.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Menu from "../../components/Menu/Menu";
+import "./ModelDetail.css";
 
 const ModelDetail = () => {
   const { id } = useParams();
@@ -20,9 +21,51 @@ const ModelDetail = () => {
   return (
     <div>
       <Menu />
-      <h1>{model.name}</h1>
-      <p>Year: {model.year}</p>
-      <p>Price: ${model.price}</p>
+      <div className="model-detail-hero">
+        <div className="container container-custom">
+          <div className="row">
+            <div className="col-12 col-md-6 model-detail-hero__image">
+              <div className="model-detail-hero-image-container">
+                <img src={model.photo} alt={model.name} className="img-fluid" />
+              </div>
+            </div>
+            <div className="col-12 col-md-6 model-detail-hero__info">
+              <div>
+                <span className="model-detail-hero__segment">{model.segment}</span>
+                <h1 className="model-detail-hero__name">{model.name}</h1>
+                <p className="model-detail-hero__p" dangerouslySetInnerHTML={{ __html: model.description }}></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="model-detail-carrusel">
+        Carrusel
+      </div>
+      <div className="model-detail-info">
+        <div className="container container-custom">
+          <div className="row">
+            <div className="col-6">Texto</div>
+            <div className="col-6">Image</div>
+          </div>
+        </div>
+      </div>
+      <div className="model-detail-info">
+        <div className="container container-custom">
+          <div className="row">
+            <div className="col-6">Image</div>
+            <div className="col-6">Texto</div>
+          </div>
+        </div>
+      </div>
+      <div className="model-detail-info">
+        <div className="container container-custom">
+          <div className="row">
+            <div className="col-6">Texto</div>
+            <div className="col-6">Image</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
