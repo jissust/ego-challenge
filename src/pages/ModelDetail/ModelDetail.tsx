627778a1fdcdccae065691f481456ff3fce2bbde
jesustissera <jesustissera@gmail.com>
2026-01-12 23:06:36 -0300

    arreglo errores de tipado y problema con llamado a css para swiper

--- src/pages/ModelDetail/ModelDetail.tsx
+++ src/pages/ModelDetail/ModelDetail.tsx
@@ -53,39 +53,39 @@ const ModelDetail = () => {
           <div className="row">
             <div className="col-12 col-md-6 model-detail-hero__image">
               <div className="model-detail-hero-image-container">
-                <img src={model.photo} alt={model.name} className="img-fluid" onLoad={ () => setImageLoaded(true)} />
+                <img src={model?.photo} alt={model?.name} className="img-fluid" onLoad={ () => setImageLoaded(true)} />
               </div>
             </div>
             <div className="col-12 col-md-6 model-detail-hero__info">
               <div>
                 <span className="model-detail-hero__segment">
-                  {model.segment}
+                  {model?.segment}
                 </span>
-                <h1 className="model-detail-hero__name">{model.name}</h1>
+                <h1 className="model-detail-hero__name">{model?.name}</h1>
                 <p
                   className="model-detail-hero__p"
-                  dangerouslySetInnerHTML={{ __html: model.description }}
+                  dangerouslySetInnerHTML={{ __html: model?.description ?? "" }}
                 ></p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <Carousel items={model.model_features} />
+      <Carousel items={model?.model_features} />
       <div className="model-detail-info">
         <div className="container container-custom">
           <div className="row">
             <div className="col-12 col-md-6 d-flex justify-content-center align-items-center order-2 order-md-1">
               <div>
-                <h2 className="model-detail-info__h2">{model.model_highlights[0].title}</h2>
-                <p  className="model-detail-info__p" dangerouslySetInnerHTML={{ __html: model.model_highlights[0].content }}></p>
+                <h2 className="model-detail-info__h2">{model?.model_highlights[0].title}</h2>
+                <p  className="model-detail-info__p" dangerouslySetInnerHTML={{ __html: model?.model_highlights[0]?.content ?? "" }}></p>
               </div>
             </div>
             <div className="col-12 col-md-6 order-1 order-md-2">
               <div>
                 <img
-                  src={model.model_highlights[0].image}
-                  alt={model.model_highlights[0].title}
+                  src={model?.model_highlights[0].image}
+                  alt={model?.model_highlights[0].title}
                   className="img-fluid"/>
               </div>
             </div>
@@ -98,16 +98,16 @@ const ModelDetail = () => {
             <div className="col-12 col-md-6">
               <div>
                 <img
-                  src={model.model_highlights[1].image}
-                  alt={model.model_highlights[1].title}
+                  src={model?.model_highlights[1].image}
+                  alt={model?.model_highlights[1].title}
                   className="img-fluid"
                 />
               </div>
             </div>
             <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
               <div>
-                <h2 className="model-detail-info__h2">{model.model_highlights[1].title}</h2>
-                <p className="model-detail-info__p" dangerouslySetInnerHTML={{ __html: model.model_highlights[1].content }}></p>
+                <h2 className="model-detail-info__h2">{model?.model_highlights[1].title}</h2>
+                <p className="model-detail-info__p" dangerouslySetInnerHTML={{ __html: model?.model_highlights[1]?.content ?? "" }}></p>
               </div>
             </div>
           </div>
